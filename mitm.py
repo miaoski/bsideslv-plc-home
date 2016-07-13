212b38dcad82ae2f2944a64ec20da695cc7f1313
miaoski <lin.zhemin@gmail.com>
2016-07-13 22:25:48 +0800

    object based action piles

--- mitm.py
+++ mitm.py
@@ -1,5 +1,5 @@
 # -*- coding: utf8 -*-
-# MITM, client to modbus #1 and server of modbus #2
+# MITM, client to modbus #1 and server of modbus #2, for level-2.
 # Providing a bit fuzzing / procrastination
 # It is based on "tick".  Think about turn-based game.
 
@@ -8,62 +8,93 @@ from time import sleep
 SERVER = '192.168.42.1'
 MYIP = '192.168.42.2'
 TICK_TIME = 1       # 1 tick = 1 second
-CUR_TICK = 0        # Greenwich mean tick :-)
+GMTICK = 0          # Greenwich mean tick :-)
 NUMBER_DI = 4       # Number of DI
-MAX_PILES = 5       # t-5 = readings to output
-                    # t-1 = waiting for ticks
-                    # t   = the newest readings, compared with t-1
 
-memory = []         # piles in tuple (reading when T=t, ticking)
+S_STRUCTURE = []    # readings in modbus #2
+D_STRUCTURE = []    # ground truth in modbus #1
+ACTIONS = []        # pile of actions to execute per tick
 
-def dft():
-    return 1        # changes after 1 tick
+class Delayed:
+    def __init__(self, pin, sv, dv, ticks):
+        self.pin = pin
+        self.sv = sv
+        self.dv = dv
+        self.ticks = ticks
+    def __call__(self):
+        global S_STRUCTURE
+        if self.ticks == 0:
+            print 'pin %d : %d => %d' % (self.pin, self.sv, self.dv)
+            S_STRUCTURE[self.pin] = self.dv
+            return None
+        self.ticks -= 1
+        return self
+    def __repr__(self):
+        return 'Delayed(pin %d: %d=>%d)' % (self.pin, self.sv, self.dv)
 
-def slw():
-    return 3        # changes after 3 ticks
-
-def rnd():
-    "Random should not exceed MAX_PILES"
-    from random import randint
-    return randint(1, MAX_PILES-1)
+from random import randint
+dft = lambda p, s, d: Delayed(p, s, d, 1)
+slw = lambda p, s, d: Delayed(p, s, d, 3)
+rnd = lambda p, s, d: Delayed(p, s, d, randint(1, 5))  # Max 5 ticks
 
 # function pointers to call when a pin changes value
-pin_change_p = [dft, dft, slw, rnd, dft, dft, dft, dft, dft, dft]
-
-
-def pile_up(newvals):
-    global memory
-    if len(newvals) != NUMBER_DI:
-        raise ValueError, "Piled readings should have %d items" % NUMBER_DI
-    if len(memory) == 0:                # pile up new things
-        for i in range(MAX_PILES):
-            memory.append((newvals[:], [f() for f in pin_change_p]))
-    else:
-        memory.append((newvals[:], [f() for f in pin_change_p]))
-    dump_memory()
+pin_change = [slw, dft, slw, rnd, dft, dft, dft, dft, dft, dft]
 
 
 def dump_memory():
-    for i in range(len(memory)):
-        print 'meomry %d:' % i, memory[i]
+    print 'Tick   :', GMTICK
+    print 'S / D  :', S_STRUCTURE, '\t', D_STRUCTURE
+    print 'Actions:', ACTIONS
 
 
-def emit_readings():
-    global CUR_TICK, memory
-    CUR_TICK += 1
-    print 'Tick advanced to %d' % CUR_TICK,
-    t = memory[0][1]
-    emit = []
-    for i in range(NUMBER_DI):
-        emit.append(memory[t[i]][0][i])
-    print emit
-    memory = memory[-MAX_PILES:]        # remove the oldest
+def compare_action():
+    for i in range(len(D_STRUCTURE)):
+        if S_STRUCTURE[i] != D_STRUCTURE[i]:
+            ACTIONS.append(pin_change[i](i, S_STRUCTURE[i], D_STRUCTURE[i]))
+
+
+def tick():
+    global GMTICK, ACTIONS
+    GMTICK += 1
+    ACTIONS = filter(lambda x: x(), ACTIONS)
+    dump_memory()
 
 
 if __name__ == '__main__':
-    v = 0
-    while True:
-        v = abs(v-1)
-        pile_up([v, 0, 0, 1])
-        emit_readings()
-        sleep(1)
+    D_STRUCTURE = [1, 0, 0, 1]
+    S_STRUCTURE = [1, 0, 0, 1]
+    compare_action()
+    tick()
+    S_STRUCTURE = [0, 0, 0, 1]
+    compare_action()
+    tick()
+    S_STRUCTURE = [0, 0, 0, 1]
+    compare_action()
+    tick()
+    S_STRUCTURE = [0, 0, 0, 1]
+    compare_action()
+    tick()
+    S_STRUCTURE = [0, 0, 0, 1]
+    compare_action()
+    tick()
+    S_STRUCTURE = [0, 0, 0, 1]
+    compare_action()
+    tick()
+    S_STRUCTURE = [0, 0, 0, 1]
+    compare_action()
+    tick()
+    S_STRUCTURE = [1, 1, 0, 1]
+    compare_action()
+    tick()
+    D_STRUCTURE = [0, 0, 0, 1]
+    compare_action()
+    tick()
+    tick()
+    tick()
+    tick()
+    tick()
+    tick()
+    tick()
+    tick()
+    tick()
+    tick()
