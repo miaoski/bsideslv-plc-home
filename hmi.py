882216411e21d0e77bd4c7cd084807404e2c73fd
miaoski <lin.zhemin@gmail.com>
2016-07-14 16:47:04 +0800

    Add ModBus #2 to HMI

--- hmi.py
+++ hmi.py
@@ -10,25 +10,39 @@ import sys
 
 client = None   # ModBus client
 
-def read_di():
-    num_di = 17
-    rr = client.read_discrete_inputs(1, num_di)
-    rr = rr.bits[:num_di]
+def read_di(num = 20):
+    rr = client.read_discrete_inputs(1, num)
+    rr = rr.bits[:num]
     di = ['0', ] + ['1' if x else '0' for x in rr]    # No GPIO 1 on RPi
     return di
 
 
-def read_re():
-    di = ['%0.2f' % (random.random()*100) for _ in range(2)]
+def read_co(num = 20):
+    rr = client.read_coils(1, num)
+    rr = rr.bits[:num]
+    di = ['0', ] + ['1' if x else '0' for x in rr]
+    return di
+
+
+def read_hr(num = 10):
+    rr = client.read_holding_registers(1, num)
+    rr = rr.registers[:num]
+    di = map(str, rr)
     return di
 
 
 class wsApp(WebSocketApplication):
     def on_open(self):
         while True:
-            di = read_di()
-            re = read_re()
-            self.ws.send(','.join(di + re))
+            try:
+                di = read_di()
+                co = read_co()
+                hr = read_hr()
+            except:
+                print 'Exception.  Wait for next run.'
+                gevent.sleep(1)
+                continue
+            self.ws.send('\n'.join((','.join(di), ','.join(co), ','.join(hr))))
             gevent.sleep(0.3)
 
     def on_close(self, reason):
