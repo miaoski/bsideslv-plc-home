89b20c18dc32d2b3e29bc33244f19bc6784a11ae
miaoski <lin.zhemin@gmail.com>
2016-07-15 16:10:23 +0800

    Fix start address
    There is a bug in ModbusTcpClient.  Spent an hour to dig it out ...

--- level-1.py
+++ level-1.py
@@ -18,33 +18,33 @@ client = ModbusTcpClient('192.168.42.1')    # 192.168.42.1 is our ground truth
 TIME_TO_COPY = 10               # Copy from truth every 10 seconds
 
 SLAVE_ID = 0x00
-DI_NUM = 100
-CO_NUM = 100
-HR_NUM = 100
-IR_NUM = 100
+DI_NUM = 20
+CO_NUM = 20
+HR_NUM = 5
+IR_NUM = 5
 
 def copy_modbus_source(a):
-    context  = a[0]
+    context = a[0]
     try:
-        rr = client.read_discrete_inputs(0, DI_NUM - 1)
+        rr = client.read_discrete_inputs(1, DI_NUM)
         context[SLAVE_ID].store['d'] = ModbusSequentialDataBlock(1, rr.bits)
     except:
         log.warn('Cannot read DI')
     try:
-        rr = client.read_coils(0, CO_NUM - 1)
+        rr = client.read_coils(1, CO_NUM)
         context[SLAVE_ID].store['c'] = ModbusSequentialDataBlock(1, rr.bits)
     except:
         log.warn('Cannot read CO')
     try:
-        rr = client.read_holding_registers(0, HR_NUM - 1)
+        rr = client.read_holding_registers(1, HR_NUM)
         context[SLAVE_ID].store['h'] = ModbusSequentialDataBlock(1, rr.registers)
     except:
         log.warn('Cannot read HR')
     try:
-        rr = client.read_input_registers(0, IR_NUM - 1)
+        rr = client.read_input_registers(1, IR_NUM)
         context[SLAVE_ID].store['i'] = ModbusSequentialDataBlock(1, rr.registers)
     except:
-        log.warn('Cannot read HR')
+        log.warn('Cannot read IR')
     log.info('Copied from modbus server #1')
 
 
